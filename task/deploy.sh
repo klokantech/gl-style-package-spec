8a1d4b56bea8bddac9e0d03ca9df95c2d2075556
Jiri Kozel <jirik@users.noreply.github.com>
2016-12-07 10:23:04 +0100

    Add `set -ev` to travis
    https://docs.travis-ci.com/user/customizing-the-build#Implementing-Complex-Build-Steps

--- task/deploy.sh
+++ task/deploy.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
+# https://docs.travis-ci.com/user/customizing-the-build#Implementing-Complex-Build-Steps
+set -ev
 
 npm install -g mapbox-gl-style-spec
-#npm install fontnik
 npm install spritezero-cli
 npm install fs-extra
 
 gl-style-validate ../style.json
 rm -rf build
-mkdir -p build
-#rm -rf tmp
-#mkdir -p tmp/ttf
-#wget https://fonts.google.com/download?family=Open%20Sans -O tmp/ttf/open-sans.zip
-#unzip tmp/ttf/open-sans.zip -d tmp/ttf/open-sans
-#mkdir -p "build/fonts/Open Sans Bold"
-#mkdir -p "build/fonts/Open Sans Italic"
-#mkdir -p "build/fonts/Open Sans Regular"
-#mkdir -p "build/fonts/Open Sans Semibold"
-#./node_modules/.bin/build-glyphs tmp/ttf/open-sans/OpenSans-Bold.ttf "build/fonts/Open Sans Bold"
-#./node_modules/.bin/build-glyphs tmp/ttf/open-sans/OpenSans-Italic.ttf "build/fonts/Open Sans Italic"
-#./node_modules/.bin/build-glyphs tmp/ttf/open-sans/OpenSans-Regular.ttf "build/fonts/Open Sans Regular"
-#./node_modules/.bin/build-glyphs tmp/ttf/open-sans/OpenSans-Semibold.ttf "build/fonts/Open Sans Semibold"
+mkdir build
 ./node_modules/.bin/spritezero build/sprite ../icons/
 ./node_modules/.bin/spritezero --retina build/sprite@2x ../icons/
 node task/index.js
