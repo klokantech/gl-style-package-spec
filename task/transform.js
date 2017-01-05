80e4d75797f209086f1eeae1c5925f7fded6db5b
jirik <jiri.kozel.cz@gmail.com>
2017-01-05 08:42:13 +0100

    Fix code style in transform.js

--- task/transform.js
+++ task/transform.js
@@ -1,64 +1,64 @@
 
 exports.adjustStyleForCdn = function(opts) {
-	
-var style = opts.style;
 
-delete style.created;
-delete style.draft;
-delete style.id;
-delete style.modified;
-delete style.owner;
+	var style = opts.style;
 
-if (style.sources['openmaptiles']) {
-  style.sources['openmaptiles'] = {
-    "type": "vector",
-    "url": "https://free.tilehosting.com/data/v3.json?key=tXiQqN3lIgskyDErJCeY"
-  }
-}
+	delete style.created;
+	delete style.draft;
+	delete style.id;
+	delete style.modified;
+	delete style.owner;
 
-if(opts.needSprite) {
-  var slug = opts.slug.split('/');
-  var user = slug[0];
-  var repo = slug[1];
-  style.sprite = "https://"+user+".github.io/"+repo+"/sprite";
-} else {
-  delete style.sprite;
-}
+	if (style.sources['openmaptiles']) {
+	  style.sources['openmaptiles'] = {
+	    "type": "vector",
+	    "url": "https://free.tilehosting.com/data/v3.json?key=tXiQqN3lIgskyDErJCeY"
+	  }
+	}
 
-style.glyphs = "https://fonts.openmaptiles.org/{fontstack}/{range}.pbf";
+	if(opts.needSprite) {
+	  var slug = opts.slug.split('/');
+	  var user = slug[0];
+	  var repo = slug[1];
+	  style.sprite = "https://"+user+".github.io/"+repo+"/sprite";
+	} else {
+	  delete style.sprite;
+	}
 
-style.layers.forEach(function(layer) {
-  if(layer.layout && layer.layout['text-font']) {
-    layer.layout['text-font'] = layer.layout['text-font'].slice(0,1);
-  }
-});
+	style.glyphs = "https://fonts.openmaptiles.org/{fontstack}/{range}.pbf";
+
+	style.layers.forEach(function(layer) {
+	  if(layer.layout && layer.layout['text-font']) {
+	    layer.layout['text-font'] = layer.layout['text-font'].slice(0,1);
+	  }
+	});
 
 };
 
 
 exports.adjustStyleForLocal = function(opts) {
-	
-var style = opts.style;
 
-delete style.created;
-delete style.draft;
-delete style.id;
-delete style.modified;
-delete style.owner;
+	var style = opts.style;
 
-if (style.sources['openmaptiles']) {
-  style.sources['openmaptiles'] = {
-      "type": "vector",
-      "url": "mbtiles://{openmaptiles}"
-  }
-}
+	delete style.created;
+	delete style.draft;
+	delete style.id;
+	delete style.modified;
+	delete style.owner;
 
-if(opts.needSprite) {
-  style.sprite = "{styleJsonFolder}/sprite";
-} else {
-  delete style.sprite;
-}
+	if (style.sources['openmaptiles']) {
+	  style.sources['openmaptiles'] = {
+	      "type": "vector",
+	      "url": "mbtiles://{openmaptiles}"
+	  }
+	}
 
-style.glyphs = "{fontstack}/{range}.pbf";
+	if(opts.needSprite) {
+	  style.sprite = "{styleJsonFolder}/sprite";
+	} else {
+	  delete style.sprite;
+	}
+
+	style.glyphs = "{fontstack}/{range}.pbf";
 
 };
