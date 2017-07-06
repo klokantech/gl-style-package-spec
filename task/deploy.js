931d04a843e7af483ad6d59518d5f4c5ef14f5c4
Jiri Kozel <jirik@users.noreply.github.com>
2017-07-06 20:15:05 +0200

    Add language fallback option (#20)
    Related to https://github.com/openmaptiles/openmaptiles/issues/211

--- task/deploy.js
+++ task/deploy.js
@@ -4,12 +4,27 @@ var transform = require('./transform.js');
 
 fs.copySync('index.html', 'build/index.html');
 
-var stylePath = '../style.json';
 
 var needSprite = fs.existsSync('../icons');
 var slug = process.env.TRAVIS_REPO_SLUG;
 
+var stylePath = '../style.json';
 var styleStr = fs.readFileSync(stylePath, 'utf8');
+
+var langCfgPath = '../lang-fallback.json';
+if(fs.existsSync(langCfgPath)) {
+  var langCfgStr = fs.readFileSync(langCfgPath, 'utf8');
+  var langCfg = JSON.parse(langCfgStr);
+}
+
+var style = JSON.parse(styleStr);
+transform.adjustStyleForCdn({
+  style: style,
+  needSprite: needSprite,
+  slug: slug,
+  langCfg: langCfg
+});
+fs.writeFileSync('build/style-cdn.json', JSON.stringify(style, null, 2), 'utf8');
 
 var style = JSON.parse(styleStr);
 transform.adjustStyleForCdn({
@@ -17,14 +32,22 @@ transform.adjustStyleForCdn({
   needSprite: needSprite,
   slug: slug
 });
-fs.writeFileSync('build/style-cdn.json', JSON.stringify(style, null, 2), 'utf8');
+fs.writeFileSync('build/style-cdn-undecorated.json', JSON.stringify(style, null, 2), 'utf8');
+
+style = JSON.parse(styleStr);
+transform.adjustStyleForLocal({
+  style: style,
+  needSprite: needSprite,
+  langCfg: langCfg
+});
+fs.writeFileSync('build/style-local.json', JSON.stringify(style, null, 2), 'utf8');
 
 style = JSON.parse(styleStr);
 transform.adjustStyleForLocal({
   style: style,
   needSprite: needSprite
 });
-fs.writeFileSync('build/style-local.json', JSON.stringify(style, null, 2), 'utf8');
+fs.writeFileSync('build/style-local-undecorated.json', JSON.stringify(style, null, 2), 'utf8');
 
 style = JSON.parse(styleStr);
 transform.adjustStyleForMapbox({
