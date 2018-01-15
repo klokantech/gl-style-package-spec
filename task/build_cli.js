55537593db9ee8ebddec9f2f8b6cfa86efd10976
jlaville <j.laville@qwant.com>
2018-01-15 10:40:12 +0100

    fix cr

--- task/build_cli.js
+++ task/build_cli.js
@@ -26,41 +26,37 @@ const args = yargs
   .argv
 
 const buildDir = path.join(args.style_dir, 'build')
-
-/*
-fs.removeSync(buildDir);
-
-fs.mkdirsSync(buildDir);
-
-if (needSprite){
-  //TODO : build sprite and sprite retina here, instead of in the bash script
-}*/
-
-const stylePath = path.join(args.style_dir,'style.json');
-
-const confStr = fs.readFileSync(args.conf, 'utf8');
-const styleStr = fs.readFileSync(stylePath, 'utf8');
-
-const jsonconf = JSON.parse(confStr);
+const stylePath = path.join(args.style_dir,'style.json')
+const confStr = fs.readFileSync(args.conf, 'utf8')
+const styleStr = fs.readFileSync(stylePath, 'utf8')
+const style = JSON.parse(styleStr)
+const jsonconf = JSON.parse(confStr)
 
 let options = {
   styleDir : args.style_dir,
   conf : jsonconf,
   outPath : buildDir
 }
+
 mkdirp(buildDir)
-const builtStyle = build(styleStr, options)
-outPath = path.join(buildDir,'built-style.json');
-fs.writeFileSync(outPath, JSON.stringify(builtStyle, null, 2), 'utf8');
+options.output = 'production'
+const builtStyle = build(style, options)
+outPath = path.join(buildDir,'built-style.json')
+fs.writeFileSync(outPath, builtStyle, 'utf8')
 
-const builtStyleDebug = build(styleStr, options)
-outPath = path.join(buildDir,'built-style-debug.json');
-fs.writeFileSync(outPath, JSON.stringify(builtStyleDebug, null, 2), 'utf8');
+console.log(">>")
+console.log(buildDir)
 
-const builtStyleOmt = build(styleStr, options)
-outPath = path.join(buildDir,'style-omt.json');
-fs.writeFileSync(outPath, JSON.stringify(builtStyleOmt, null, 2), 'utf8');
+options.output = 'debug'
+const builtStyleDebug = build(style, options)
+outPath = path.join(buildDir,'built-style-debug.json')
+fs.writeFileSync(outPath, JSON.stringify(JSON.parse(builtStyleDebug), null, 2), 'utf8') //trick to restore formatting
+
+options.output = 'omt'
+const builtStyleOmt = build(style, options)
+outPath = path.join(buildDir,'style-omt.json')
+fs.writeFileSync(outPath, builtStyleOmt, 'utf8')
 
 
-fs.copySync(path.join(__dirname, '..', 'compare.html'), path.join(buildDir, 'compare.html'));
-fs.copySync(path.join(__dirname, '..', 'debug.html'), path.join(buildDir, 'debug.html'));
+fs.copySync(path.join(__dirname, '..', 'compare.html'), path.join(buildDir, 'compare.html'))
+fs.copySync(path.join(__dirname, '..', 'debug.html'), path.join(buildDir, 'debug.html'))
