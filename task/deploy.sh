a3c627abb5fba241595ae4ea16e4516335c4789a
jirik <jirik@users.noreply.github.com>
2018-01-18 13:57:22 +0100

    Generate previews before first push

--- task/deploy.sh
+++ task/deploy.sh
@@ -17,15 +17,9 @@ node task/deploy.js
 git config --global user.email "openmaptiles@klokantech.com"
 git config --global user.name "OpenMapTiles Travis"
 
-# deploy
-cd build
-git init
-git add .
-git commit -m "Deploy to Github Pages"
-echo "Pushing"
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
-echo "Pushed"
-
+# hack: renders preview of last-but-one commit
+# doing it after first push ended with
+#   [ERROR] {mbgl-render}[Style]: Failed to load sprite: HTTP status code 404
 mkdir preview
 cd preview
 echo "Pulling"
@@ -33,8 +27,12 @@ docker pull klokantech/thumbnail-gl
 echo "Generating thumbnails"
 docker run -v $(pwd):/data klokantech/thumbnail-gl "https://raw.githubusercontent.com/${TRAVIS_REPO_SLUG}/gh-pages/style-cdn.json"
 cd ..
-git add preview/
-git commit -m "Add previews to Github Pages"
+
+# deploy
+cd build
+git init
+git add .
+git commit -m "Deploy to Github Pages"
 echo "Pushing"
-git push --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
 echo "Pushed"
